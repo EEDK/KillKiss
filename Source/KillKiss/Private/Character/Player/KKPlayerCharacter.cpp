25dbe146833f9ea4fb6b65a6a567c99a088fc749
EDK <kde9604@inu.ac.kr>
2024-12-07 16:10:58 +0900

    Input binding done

--- Source/KillKiss/Private/Character/Player/KKPlayerCharacter.cpp
+++ Source/KillKiss/Private/Character/Player/KKPlayerCharacter.cpp
@@ -5,7 +5,12 @@
 
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Controllers/KKPlayerController.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/Input/KKInputComponent.h"
+#include "DataAssets/DataAsset_InputConfig.h"
+#include "KKGameplayTags.h"
 
 AKKPlayerCharacter::AKKPlayerCharacter()
 {
@@ -25,4 +30,59 @@ AKKPlayerCharacter::AKKPlayerCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 	FollowCamera->bUsePawnControlRotation = false;
+}
+
+void AKKPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	checkf(InputConfigDataAsset, TEXT("Forgot to assign a valid data asset as input config"));
+	ULocalPlayer* LocalPlayer = GetController<AKKPlayerController>()->GetLocalPlayer();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
+	UKKInputComponent* KKInputComponent = CastChecked<UKKInputComponent>(PlayerInputComponent);
+
+	KKInputComponent->BindNativeInputAction(InputConfigDataAsset, KKGameplayTags::InputTag_Move,
+	                                        ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+
+	KKInputComponent->BindNativeInputAction(InputConfigDataAsset, KKGameplayTags::InputTag_Look,
+	                                        ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
+}
+
+void AKKPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AKKPlayerCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+	const FRotator MovementRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+	if (MovementVector.Y != 0.f)
+	{
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+	}
+	if (MovementVector.X != 0.f)
+	{
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AKKPlayerCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	if (LookAxisVector.X != 0.f)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+	}
+	if (LookAxisVector.Y != 0.f)
+	{
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
--- Source/KillKiss/Public/Character/Player/KKPlayerCharacter.h
+++ Source/KillKiss/Public/Character/Player/KKPlayerCharacter.h
@@ -6,6 +6,8 @@
 #include "Character/KKCharacterBase.h"
 #include "KKPlayerCharacter.generated.h"
 
+struct FInputActionValue;
+class UDataAsset_InputConfig;
 class USpringArmComponent;
 class UCameraComponent;
 
@@ -22,11 +24,23 @@ public:
 	AKKPlayerCharacter();
 
 protected:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	virtual void BeginPlay() override;
 
 private:
-	UPROPERTY(EditDefaultsOnly)
-	TObjectPtr<USpringArmComponent> CameraBoom;
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
 
-	UPROPERTY(EditDefaultsOnly)
-	TObjectPtr<UCameraComponent> FollowCamera;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+#pragma endregion
+
+#pragma region Inputs
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+#pragma endregion
 };
