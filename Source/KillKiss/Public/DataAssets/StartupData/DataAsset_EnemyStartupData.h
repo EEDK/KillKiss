d4e254c1e3859801e6bd110d1eaf44968be8a200
EDK <kde960419@gmail.com>
2025-01-01 15:32:18 +0900

    Add Skeleton Warrior

---
+++ Source/KillKiss/Public/DataAssets/StartupData/DataAsset_EnemyStartupData.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/StartupData/DataAsset_StartupDataBase.h"
+#include "DataAsset_EnemyStartupData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KILLKISS_API UDataAsset_EnemyStartupData : public UDataAsset_StartupDataBase
+{
+	GENERATED_BODY()
+};
